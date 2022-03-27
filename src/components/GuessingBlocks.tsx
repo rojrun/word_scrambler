d37730b48d68da39a3011b3bbd0681bdb7b3ea3d
rojrun <runroj@gmail.com>
2022-03-27 15:02:49 -0700

    writers block. having trouble making a row with word and space

--- src/components/GuessingBlocks.tsx
+++ src/components/GuessingBlocks.tsx
@@ -34,31 +34,48 @@ const GuessingBlocks = ({sentence = []}: Props) => {
     } 
   }
 
+  // return (
+  //   <div ref={inputRef}>
+  //     {
+  //       sentence.map((nestedArr: string[], wordInd: number) => {
+  //         return (
+  //           <div className="row" key={wordInd + 1}>
+  //             {
+  //               nestedArr.map((char: string, charInd: number) => {
+  //                 let bgColor = "";
+  //                 if (charInd === 0 && char === " ") {
+  //                   bgColor = "bg-warning";
+  //                 }
+                
+  //                 return (
+  //                   <input
+  //                     className={`col text-center m-2 ${bgColor}`} type="text" id="guess" autoComplete="off"
+  //                     onChange={(e) => handleGuess(e, wordInd, charInd)} key={charInd + 1} maxLength={1}
+  //                   />
+  //                 );
+  //               })
+  //             }
+  //           </div>
+  //         );
+  //       })      
+  //     }
+  //   </div>
+  // );
+
+  const makeRow = (ind: number) => {return <div key={ind + 1} className="row"></div>};
+  const makeInput = (ind: number) => {return <input className="col text-center m-2" type="text" id="guess" autoComplete="off" key={ind + 1} maxLength={1} />};
+
+  const row = sentence.map((nestedArr: string[], wordInd: number) => {
+    if (nestedArr[0] !== " ") {
+      
+    }
+  });
+
+  
+
   return (
     <div ref={inputRef}>
-      {
-        sentence.map((nestedArr: string[], wordInd: number) => {
-          return (
-            <div className="row" key={wordInd + 1}>
-              {
-                nestedArr.map((char: string, charInd: number) => {
-                  let bgColor = "";
-                  if (charInd === 0 && char === " ") {
-                    bgColor = "bg-warning";
-                  }
-                
-                  return (
-                    <input
-                      className={`col text-center m-2 ${bgColor}`} type="text" id="guess" autoComplete="off"
-                      onChange={(e) => handleGuess(e, wordInd, charInd)} key={charInd + 1} maxLength={1}
-                    />
-                  );
-                })
-              }
-            </div>
-          );
-        })      
-      }
+      {row}
     </div>
   );
 }
