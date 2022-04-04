8babed85bddef51c029b4c6b72ed2cd56e554b4c
rojrun <runroj@gmail.com>
2022-04-03 20:01:00 -0700

    broken

--- src/components/GuessingBlocks.tsx
+++ src/components/GuessingBlocks.tsx
@@ -8,98 +8,84 @@ interface Props {
 
 // Render blocks based on shape of Sentence array
 const GuessingBlocks = ({sentence = [], setScore, score}: Props) => {
+  const valuesInitialState: string[][] = sentence.map((word: string[]) => word.map(() => ""));
+  const [values, setValues] = useState<string[][]>(valuesInitialState);
   const [cursor, setCursor] = useState([0, 0]);
-  // const inputRef = useRef<HTMLInputElement>(null);
-  const inputRef:any = useMemo(
+  const [correct, setCorrect] = useState<number>(0);
+  const [totalInputs, setTotalInputs] = useState<number>(0);
+  const inputRef: any = useMemo(
     () => sentence.map((word) => word.map((_) => createRef())),
     [sentence]
   );
-  const [values, setValues] = useState<string[][]>(sentence.map((word: string[]) => word.map((_char: string) => "")));
-  const [correct, setCorrect] = useState<number>(0);
-  const [totalInputs, setTotalInputs] = useState<number>(0);
-  
-  // const handleGuess = (e: React.ChangeEvent<HTMLInputElement>, wordIn: number, charIn: number): void => {
-  //   const guess = e.target.value;
-  //   const element = e.target;
-  //   if (guess === sentence[wordIn][charIn]) {
-  //     if (element.classList.contains("bg-warning")) {
-  //       element.classList.remove("bg-warning");
-  //       element.className += " bg-success text-white";
-  //     } else {
-  //       element.className += "bg-success text-white";
-  //     }
-  //     setCorrect(correct + 1);
-  //     (element.nextElementSibling as HTMLElement)?.focus();
-      
-  //     if (correct === totalInputs) {
-  //       setScore(score + 1);
-  //     }
-  //   } 
-  // }
+  const buttonRef: any = useRef();
+  console.log("buttonRef: ", buttonRef.current);
   const countTotalInputs = () => {
     let total: number = 0;
     sentence.forEach((array: string[]) => total += array.length);
     setTotalInputs(total);
   }
 
-  const handleGuess = (char: string, wordInd: number, charInd: number) => {
-    if (char !== sentence[wordInd][charInd]) return;
-    setCursor(([_wordInd, _charInd]) => 
-      _charInd + 1 < sentence[_wordInd].length
-        ? [_wordInd, _charInd + 1]
-        : [_wordInd + 1, 0]    
-    )
-    setValues((values): any => {
-      console.log("values: ", values);
-      values.map((_word, _wordInd) => {
-        console.log("_word: ", _word);
-        console.log("_wordInd: ", _wordInd);
-        if (wordInd !== _wordInd) return _word;
-        return _word.map((_char, _charInd) => {
-          if (charInd !== _charInd) return _char;
-          return char;
-        })
-      })
-    }
+  const handleGuess = (char: string, wordIndex: number, charIndex: number) => {
+    if (char === sentence[wordIndex][charIndex]) {
+      const input = inputRef[wordIndex][charIndex].current;
+      if (input.classList.contains("bg-warning")) {
+        input.classList.remove("bg-warning");
+        input.className += " bg-success text-white";
+      } else {
+        input.className += "bg-success text-white";
+      }
       
-    )
+      setCorrect(correct + 1);     
+
+      setCursor(([_wordIndex, _charIndex]) => 
+        _charIndex + 1 < sentence[_wordIndex].length
+          ? [_wordIndex, _charIndex + 1]
+          : [_wordIndex + 1, 0]    
+      );
+      
+      
+    }
+    
+    // setValues((values): any => {
+    //   values.map((_word, _wordIndex) => {
+    //     if (wordIndex !== _wordIndex) return _word;
+    //     return _word.map((_char, _charIndex) => {
+    //       if (charIndex !== _charIndex) return _char;
+    //       return char;
+    //     })
+    //   })  
+    // });
   }
 
   useEffect(() => {
-    // if (inputRef.current !== null) {
-    //   console.log("inputRef: ", inputRef.current);
-    //   (inputRef.current.firstChild?.firstChild as HTMLElement)?.focus();     
-      
-    // }
-    const [wordInd, charInd] = cursor;
-    inputRef[wordInd]?.[charInd]?.current.focus();
+    const [wordIndex, charIndex] = cursor;
+    inputRef[wordIndex]?.[charIndex]?.current.focus();
     countTotalInputs();
-    console.log("values: ", values);
-  }, [sentence, correct, cursor, inputRef]);
-
+  }, [sentence, cursor, inputRef, values]);
+  
   return (
     <div>
       <div>
         {
-          sentence.map((nestedArr: string[], wordInd: number) => {
+          sentence.map((nestedArr: string[], wordIndex: number) => {
             return (
-              <div className="row" key={wordInd}>
+              <div className="row" key={wordIndex}>
                 {
-                  nestedArr.map((char: string, charInd: number) => {
+                  nestedArr.map((char: string, charIndex: number) => {
                     let bgColor = "";
                     if (char === " ") {
                       bgColor = "bg-warning";
                     }
-                  
+                    // console.log("values: ", values);
                     return (
                       <input
                         type="text" id="guess" autoComplete="off"
                         className={`col text-center m-1 px-1 ${bgColor}`} 
-                        onChange={(e) => handleGuess(e.target.value, wordInd, charInd)}
-                        key={charInd} 
+                        onChange={(e) => handleGuess(e.target.value, wordIndex, charIndex)}
+                        key={charIndex} 
                         maxLength={1}
-                        ref={inputRef[wordInd][charInd]}
-                        value={values[wordInd][charInd]}                     
+                        ref={inputRef[wordIndex][charIndex]}
+                        // value={values[wordIndex][charIndex]}                     
                       />
                     );
                   })
@@ -111,7 +97,14 @@ const GuessingBlocks = ({sentence = [], setScore, score}: Props) => {
       </div>
       {
         correct === totalInputs ? 
-          <button type="button" className="btn btn-success px-4 m-3">Next</button>
+          <button 
+            type="button" 
+            className="btn btn-success px-4 m-3" 
+            ref={buttonRef}
+            // onClick={}
+          >
+            Next
+          </button>
           : null
       }
     </div>
